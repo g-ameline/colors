4b962fd44cd9f23f327474b17a00942344e09124
gameline <gameline_kood>
2023-09-03 20:12:11 +0300

    simpler to put all stuff as const

--- colors.go
+++ colors.go
@@ -4,111 +4,55 @@ import "fmt"
 
 const Reset = "\033[0m"
 
-var Frontground_colors = map[string]string{
-	"black":   "\033[30m",
-	"red":     "\033[31m",
-	"green":   "\033[32m",
-	"yellow":  "\033[33m",
-	"blue":    "\033[34m",
-	"magenta": "\033[35m",
-	"cyan":    "\033[36m",
-	"white":   "\033[37m",
-	"gray":    "\033[90m",
-	// light colors
-	"red_light":     "\033[91m",
-	"green_light":   "\033[92m",
-	"yellow_light":  "\033[93m",
-	"blue_light":    "\033[94m",
-	"magenta_light": "\033[95m",
-	"cyan_light":    "\033[96m",
-	"gray_light":    "\033[97m",
-}
-var FGC = Frontground_colors
-var Background_colors = map[string]string{
-	"BLACK":   "\033[40m",
-	"RED":     "\033[41m",
-	"GREEN":   "\033[42m",
-	"YELLOW":  "\033[43m",
-	"BLUE":    "\033[44m",
-	"MAGENTA": "\033[45m",
-	"CYAN":    "\033[46m",
-	"WHITE":   "\033[47m",
-	"GRAY":    "\033[100m",
-	// light colors
-	"RED_LIGHT":     "\033[101m",
-	"GREEN_LIGHT":   "\033[102m",
-	"YELLOW_LIGHT":  "\033[103m",
-	"BLUE_LIGHT":    "\033[104m",
-	"MAGENTA_LIGHT": "\033[105m",
-	"CYAN_LIGHT":    "\033[106m",
-	"GRAY_LIGHT":    "\033[107m",
-}
-var BGC = Background_colors
-var Effects = map[string]string{
-	"bold":       "\033[1m", // bold
-	"faint":      "\033[2m", // faint
-	"standout":   "\033[3m", // standout
-	"underlined": "\033[4m", // underlined
-	"blinkk":     "\033[5m", // blink
-	"reverse":    "\033[7m", // reverse
-}
+const DefaulT = "\033[39m" // default foreground
+const DEFAULT = "\033[49m" // default background
+// foreground
+const Black = "\033[30m"
+const Red = "\033[31m"
+const Green = "\033[32m"
+const Yellow = "\033[33m"
+const Blue = "\033[34m"
+const Magenta = "\033[35m"
+const Cyan = "\033[36m"
+const White = "\033[37m"
+const Gray = "\033[90m"
+const Red_light = "\033[91m"
+const Green_light = "\033[92m"
+const Yellow_light = "\033[93m"
+const Blue_light = "\033[94m"
+const Magenta_light = "\033[95m"
+const Cyan_light = "\033[96m"
+const Gray_light = "\033[97m"
 
-var All = map[string]string{
-	"off":     "\033[0m",  // off
-	"defaulT": "\033[39m", // default foreground
-	"DEFAULT": "\033[49m", // default background
+// background
+const BLACK = "\033[40m"
+const RED = "\033[41m"
+const GREEN = "\033[42m"
+const YELLOW = "\033[43m"
+const BLUE = "\033[44m"
+const MAGENTA = "\033[45m"
+const CYAN = "\033[46m"
+const WHITE = "\033[47m"
+const GRAY = "\033[100m"
+const RED_LIGHT = "\033[101m"
+const GREEN_LIGHT = "\033[102m"
+const YELLOW_LIGHT = "\033[103m"
+const BLUE_LIGHT = "\033[104m"
+const MAGENTA_LIGHT = "\033[105m"
+const CYAN_LIGHT = "\033[106m"
+const GRAY_LIGHT = "\033[107m"
 
-	"bold":        "\033[1m",  // bold
-	"faint":       "\033[2m",  // faint
-	"standoutt":   "\033[3m",  // standout
-	"underlinedl": "\033[4m",  // underlined
-	"blinkk":      "\033[5m",  // blink
-	"reversev":    "\033[7m",  // reverse
-	"hidden":      "\033[8m",  // hidden
-	"nost":        "\033[23m", // no standout
-	"noul":        "\033[24m", // no underlined
-	"nobk":        "\033[25m", // no blink
-	"norv":        "\033[27m", // no reverse
-	// colors
-	"black":   "\033[30m",
-	"BLACK":   "\033[40m",
-	"red":     "\033[31m",
-	"RED":     "\033[41m",
-	"green":   "\033[32m",
-	"GREEN":   "\033[42m",
-	"yellow":  "\033[33m",
-	"YELLOW":  "\033[43m",
-	"blue":    "\033[34m",
-	"BLUE":    "\033[44m",
-	"magenta": "\033[35m",
-	"MAGENTA": "\033[45m",
-	"cyan":    "\033[36m",
-	"CYAN":    "\033[46m",
-	"white":   "\033[37m",
-	"WHITE":   "\033[47m",
-
-	// light colors
-	"red_light":     "\033[91m",
-	"green_light":   "\033[92m",
-	"yellow_light":  "\033[93m",
-	"blue_light":    "\033[94m",
-	"magenta_light": "\033[95m",
-	"cyan_light":    "\033[96m",
-	"gray_light":    "\033[97m",
-	"RED_LIGHT":     "\033[101m",
-	"GREEN_LIGHT":   "\033[102m",
-	"YELLOW_LIGHT":  "\033[103m",
-	"BLUE_LIGHT":    "\033[104m",
-	"MAGENTA_LIGHT": "\033[105m",
-	"CYAN_LIGHT":    "\033[106m",
-	"GRAY_LIGHT":    "\033[107m",
-}
+// effects
+const Bold = "\033[1m"       // bold
+const Faint = "\033[2m"      // faint
+const Standout = "\033[3m"   // standout
+const Underlined = "\033[4m" // underlined
+const Blink = "\033[5m"      // blink
+const Reverse = "\033[7m"    // reverse
 
 func Println(content any, features ...string) {
 	for _, feature := range features {
-		if code, ok := All[feature]; ok {
-			fmt.Print(code)
-		}
+		fmt.Print(feature)
 	}
 	fmt.Print(content)
 	fmt.Println(Reset)
@@ -116,9 +60,9 @@ func Println(content any, features ...string) {
 func Color_scope(color string) {
 	fmt.Print(color)
 	defer fmt.Print(Reset)
-}
+} // to be called into curly brackets
 
-func Zeroize() { fmt.Print(Reset) }
+func Reseting() { fmt.Print(Reset) }
 
 type Colorable string
 
